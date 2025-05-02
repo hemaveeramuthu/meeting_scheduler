67ebdd6928f60d7453068d9d8eb9dc98cda1e1c3
hemadharunika <hemadharunika@gmail.com>
2025-05-02 22:27:24 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT) || 5173
+  },
   plugins: [react()],
 })
